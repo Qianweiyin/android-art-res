f4552387245ba26f3289d099d208d39d91e12d51
qianweiyin <weiyin.qian@pedesx.com>
2021-01-15 16:12:04 +0800

    旧Activity的onPause先调用，然后新Activity才启动

--- Chapter_01/app/src/main/java/com/qwy/chapter_01/MainActivity.kt
+++ Chapter_01/app/src/main/java/com/qwy/chapter_01/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.qwy.chapter_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop")
+    }
+
 }
---
+++ Chapter_01/app/src/main/java/com/qwy/chapter_01/SecondActivity.kt
@@ -0,0 +1,31 @@
+package com.qwy.chapter_01
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "SecondActivity"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second);
+        Log.e(TAG,"onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
+
+}
